27f842aa7bf519a8c7536d7550961c6cfa9f388e
Michael Grosser <development@stp-ip.net>
2017-09-10 18:41:53 +0000

    Refine and add captcha to contact fragment

--- static/js/syna.js
+++ static/js/syna.js
@@ -1,15 +1,44 @@
+var selfCheck = "checkSelf";
+
 $.validate({
-  modules: 'html5, sanitize, toggleDisabled',
+  modules: 'html5, toggleDisabled',
   validateOnEvent: true,
   onError: function($form) {
     $('form[id=' +  $form.attr('id') +'] .error').removeClass('d-none');
  },
   onSuccess: function($form) {
-    $.post($('#' + $form.attr('id')).attr('action'), $('#' + $form.attr('id')).serialize(), function(){
-      $('form[id=' +  $form.attr('id') +'] .success').removeClass('d-none');
-    }, 'json').fail(function(){
-      $('form[id=' +  $form.attr('id') +'] .error').removeClass('d-none');
-  });
+    if (selfCheck != "checkSelf") {
+      $('form[id=' +  $form.attr('id') +'] .generic-error').removeClass('d-none');
+      return false;
+    }
+    if ($('.g-recaptcha').length == 0) {
+      $.post($('#' + $form.attr('id')).attr('action'), $('#' + $form.attr('id')).serialize(), function(){
+      }, 'json')
+      .fail(function(){
+        $('form[id=' +  $form.attr('id') +'] .generic-error').removeClass('d-none');
+      })
+      .done(function(){
+        $('form[id=' +  $form.attr('id') +'] .generic-success').removeClass('d-none');
+      });
+    } else if (typeof grecaptcha != "undefined") {
+      if (grecaptcha.getResponse() != "") {
+        $.post($('#' + $form.attr('id')).attr('action'), $('#' + $form.attr('id')).serialize(), function(){
+          $('form[id=' +  $form.attr('id') +'] .success').removeClass('d-none');
+        }, 'json')
+        .fail(function(){
+          $('form[id=' +  $form.attr('id') +'] .generic-error').removeClass('d-none');
+        })
+        .done(function(){
+          $('form[id=' +  $form.attr('id') +'] .generic-success').removeClass('d-none');
+        });
+      } else {
+        grecaptcha.execute();
+      };
+    }
     return false;
-}
+  }
 });
+
+function onContactCaptcha($form) {
+  $('form.contact').submit();
+}
