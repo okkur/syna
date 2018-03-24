627fcaa73813e4f06b17fca93e17df3d67ef4d30
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2018-03-25 00:58:39 +0430

    Fix $.validate not a function error

--- static/js/syna.js
+++ static/js/syna.js
@@ -11,30 +11,20 @@ $(function() {
   }
 });
 
-$.validate({
-  modules: 'html5, toggleDisabled',
-  validateOnEvent: true,
-  onError: function($form) {
-    $('form[id=' +  $form.attr('id') +'] .generic-error').removeClass('d-none');
- },
-  onSuccess: function($form) {
-    if (selfCheck != "checkSelf") {
+if ($.validate) {
+  $.validate({
+    modules: 'html5, toggleDisabled',
+    validateOnEvent: true,
+    onError: function($form) {
       $('form[id=' +  $form.attr('id') +'] .generic-error').removeClass('d-none');
-      return false;
-    }
-    if ($('.g-recaptcha').length == 0) {
-      $.post($('#' + $form.attr('id')).attr('action'), $('#' + $form.attr('id')).serialize(), function(){
-      }, 'json')
-      .fail(function(){
+  },
+    onSuccess: function($form) {
+      if (selfCheck != "checkSelf") {
         $('form[id=' +  $form.attr('id') +'] .generic-error').removeClass('d-none');
-      })
-      .done(function(){
-        $('form[id=' +  $form.attr('id') +'] .generic-success').removeClass('d-none');
-      });
-    } else if (typeof grecaptcha != "undefined") {
-      if (grecaptcha.getResponse() != "") {
+        return false;
+      }
+      if ($('.g-recaptcha').length == 0) {
         $.post($('#' + $form.attr('id')).attr('action'), $('#' + $form.attr('id')).serialize(), function(){
-          $('form[id=' +  $form.attr('id') +'] .success').removeClass('d-none');
         }, 'json')
         .fail(function(){
           $('form[id=' +  $form.attr('id') +'] .generic-error').removeClass('d-none');
@@ -42,13 +32,25 @@ $.validate({
         .done(function(){
           $('form[id=' +  $form.attr('id') +'] .generic-success').removeClass('d-none');
         });
-      } else {
-        grecaptcha.execute();
-      };
+      } else if (typeof grecaptcha != "undefined") {
+        if (grecaptcha.getResponse() != "") {
+          $.post($('#' + $form.attr('id')).attr('action'), $('#' + $form.attr('id')).serialize(), function(){
+            $('form[id=' +  $form.attr('id') +'] .success').removeClass('d-none');
+          }, 'json')
+          .fail(function(){
+            $('form[id=' +  $form.attr('id') +'] .generic-error').removeClass('d-none');
+          })
+          .done(function(){
+            $('form[id=' +  $form.attr('id') +'] .generic-success').removeClass('d-none');
+          });
+        } else {
+          grecaptcha.execute();
+        };
+      }
+      return false;
     }
-    return false;
-  }
-});
+  });
+}
 
 function onContactCaptcha($form) {
   $('form.contact').submit();
