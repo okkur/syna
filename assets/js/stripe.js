d393e571c5361fa4166c5310921901c289d309b5
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2018-11-30 17:48:49 +0330

    Remove unused variable

--- assets/js/stripe.js
+++ assets/js/stripe.js
@@ -2,7 +2,7 @@ import $ from './helpers/jq-helpers';
 import Validator from 'form-validator-simple';
 
 function initFormValidation(form, onSuccess = () => false) {
-  const validator = new Validator({
+  new Validator({
     errorTemplate: '<span class="help-block form-error">%s</span>',
     onFormValidate: (isFormValid, form) => {
       form.querySelector('button').disabled = !isFormValid
