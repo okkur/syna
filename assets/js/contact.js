2053338642c56b2af5904f92a8b45d2193f202d2
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2019-02-24 14:22:20 +0330

    Fix contact form subscriber duplicating text on multiple events

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -85,5 +85,8 @@ window.syna.stream.subscribe('contact:update', function({ name, email, phone, me
   $('input[name=name]')[0].scrollIntoView({behavior: "instant", block: "center"});
   $('input[name=email]').attr('value', email || null);
   $('input[name=phone]').attr('value', phone || null);
-  $('textarea[name=message]').$nodes.forEach(node => node.appendChild(document.createTextNode(message || '')));
+  $('textarea[name=message]').$nodes.forEach(node => {
+    node.innerHTML = '';
+    node.appendChild(document.createTextNode(message || ''));
+  });
 });
