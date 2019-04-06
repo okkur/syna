3febdb31feced96bedef1dd661f98bde8c415966
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2019-04-06 22:03:12 +0430

    Change pricing event name and params

--- assets/js/pricing.js
+++ assets/js/pricing.js
@@ -1,9 +1,9 @@
 import $ from './helpers/jq-helpers';
 
-window.syna.stream.subscribe('plan:change', function({ plan }) {
+window.syna.stream.subscribe('plan:select', function({ plan }) {
   const pricingPlan = $(`[data-pricing-plan="${plan}"]`);
   const primaryAction = pricingPlan.$('[data-primary-action]');
-console.log("EVENT", "plan:change", {pricingPlan,primaryAction})
+
   $('.pricing-plan').removeClass('selected');
   pricingPlan.addClass('selected');
   setTimeout(() => primaryAction[0][primaryAction.attr('data-primary-action')](), 100);
