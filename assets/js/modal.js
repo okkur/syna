a38d2e0f9a04805f77a1ece604687ebb7b8a1aca
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2019-12-02 18:39:47 +0330

    Move modal html template to js asset files

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,33 +1,38 @@
 import $ from './helpers/jq-helpers';
+import ModalTemplate from './templates/modal';
 
-const modal = $('.modal');
-const dialog = $('.modal .modal-dialog');
+$('body').append(ModalTemplate);
 
-function closeDialog() {
-  $('body').removeClass('modal-open');
-  modal.removeClass('show');
-}
+setTimeout(() => {
+  const modal = $('.modal');
+  const dialog = $('.modal .modal-dialog');
 
-$('[data-dismiss="modal"]').on('click', closeDialog);
-
-modal.on('click', e => { 
-  if (!dialog[0].contains(e.target)) {
-    closeDialog()
+  function closeDialog() {
+    $('body').removeClass('modal-open');
+    modal.removeClass('show');
   }
-});
 
-(window.syna || (window.syna = {})).showModal = function({ title, subtitle, image, content, labels, size = '' }) {
-  $('body').addClass('modal-open');
-  modal.addClass('show');
-  dialog.$('.title').html(title || '');
-  dialog.$('.subtitle').html(subtitle || '');
-  dialog.$('.badge-container').html(labels || '');
-  dialog.$('img')[0].src = image;
-  if (content) {
-    dialog.$('.modal-body .content').html(content);
-    dialog.$('.modal-body .content').removeClass('hidden');
-  } else {
-    dialog.$('.modal-body .content').addClass('hidden');
+  $('[data-dismiss="modal"]').on('click', closeDialog);
+
+  modal.on('click', e => { 
+    if (!dialog[0].contains(e.target)) {
+      closeDialog()
+    }
+  });
+
+  (window.syna || (window.syna = {})).showModal = function({ title, subtitle, image, content, labels, size = '' }) {
+    $('body').addClass('modal-open');
+    modal.addClass('show');
+    dialog.$('.title').html(title || '');
+    dialog.$('.subtitle').html(subtitle || '');
+    dialog.$('.badge-container').html(labels || '');
+    dialog.$('img')[0].src = image;
+    if (content) {
+      dialog.$('.modal-body .content').html(content);
+      dialog.$('.modal-body .content').removeClass('hidden');
+    } else {
+      dialog.$('.modal-body .content').addClass('hidden');
+    }
+    dialog.removeClass('md').removeClass('lg').addClass(size);
   }
-  dialog.removeClass('md').removeClass('lg').addClass(size);
-}
+}, 0);
