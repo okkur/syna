d401c5d85d444a29983d0dd6eace8372d4947043
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2020-01-30 18:47:39 +0330

    Add support for icon in portfolio's modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,38 +1,63 @@
-import $ from './helpers/jq-helpers';
-import ModalTemplate from './templates/modal';
+import $ from "./helpers/jq-helpers";
+import ModalTemplate from "./templates/modal";
 
-$('body').append(ModalTemplate);
+$("body").append(ModalTemplate);
 
 setTimeout(() => {
-  const modal = $('.modal');
-  const dialog = $('.modal .modal-dialog');
+  const modal = $(".modal");
+  const dialog = $(".modal .modal-dialog");
 
   function closeDialog() {
-    $('body').removeClass('modal-open');
-    modal.removeClass('show');
+    $("body").removeClass("modal-open");
+    modal.removeClass("show");
   }
 
-  $('[data-dismiss="modal"]').on('click', closeDialog);
+  $('[data-dismiss="modal"]').on("click", closeDialog);
 
-  modal.on('click', e => { 
+  modal.on("click", e => {
     if (!dialog[0].contains(e.target)) {
-      closeDialog()
+      closeDialog();
     }
   });
 
-  (window.syna || (window.syna = {})).showModal = function({ title, subtitle, image, content, labels, size = '' }) {
-    $('body').addClass('modal-open');
-    modal.addClass('show');
-    dialog.$('.title').html(title || '');
-    dialog.$('.subtitle').html(subtitle || '');
-    dialog.$('.badge-container').html(labels || '');
-    dialog.$('img')[0].src = image;
-    if (content) {
-      dialog.$('.modal-body .content').html(content);
-      dialog.$('.modal-body .content').removeClass('hidden');
+  (window.syna || (window.syna = {})).showModal = function({
+    title,
+    subtitle,
+    image,
+    icon,
+    content,
+    labels,
+    size = ""
+  }) {
+    $("body").addClass("modal-open");
+    modal.addClass("show");
+    dialog.$(".title").html(title || "");
+    dialog.$(".subtitle").html(subtitle || "");
+    dialog.$("img")[0].src = image;
+
+    if (labels) {
+      dialog.$(".badge-container").removeClass("hidden");
+      dialog.$(".badge-container").html(labels || "");
     } else {
-      dialog.$('.modal-body .content').addClass('hidden');
+      dialog.$(".badge-container").addClass("hidden");
     }
-    dialog.removeClass('md').removeClass('lg').addClass(size);
-  }
+
+    if (icon) {
+      dialog.$(".icon-container").removeClass("hidden");
+      dialog.$(".icon-container").html(icon.replace(/fa-inverse/g, ""));
+    } else {
+      dialog.$(".icon-container").addClass("hidden");
+    }
+
+    if (content) {
+      dialog.$(".modal-body .content").html(content);
+      dialog.$(".modal-body .content").removeClass("hidden");
+    } else {
+      dialog.$(".modal-body .content").addClass("hidden");
+    }
+    dialog
+      .removeClass("md")
+      .removeClass("lg")
+      .addClass(size);
+  };
 }, 0);
