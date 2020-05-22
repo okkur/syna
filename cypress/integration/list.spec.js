b47da483efcaafcd157e2b9fac73df75e6aa4574
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2020-05-22 20:47:05 +0430

    Fix list item count in the list test

--- cypress/integration/list.spec.js
+++ cypress/integration/list.spec.js
@@ -1,8 +1,8 @@
 describe("List fragment", () => {
   it("should display pages from same, specific section, with subsections and with subsection leaves", () => {
     cy.visit("/dev/list");
-    cy.get("#upper-leaves article").should("have.length", 4);
-    cy.get("#branches article").should("have.length", 6);
-    cy.get("#leaves article").should("have.length", 10);
+    cy.get("#upper-leaves article").should("have.length", 2);
+    cy.get("#branches article").should("have.length", 4);
+    cy.get("#leaves article").should("have.length", 8);
   });
 });
