ab66352a48b1f522698753658f5085298d015dc3
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2020-06-24 22:22:33 +0430

    Fix faulty modal test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,25 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+function waitForResourceToLoad(fileName, type) {
+  const resourceCheckInterval = 40;
+
+  return new Cypress.Promise((resolve) => {
+    const checkIfResourceHasBeenLoaded = () => {
+      const resource = cy
+        .state('window')
+        .performance.getEntriesByType('resource')
+        .filter((entry) => !type || entry.initiatorType === type)
+        .find((entry) => entry.name.includes(fileName));
+
+      if (resource) {
+        resolve();
+
+        return;
+      }
+
+      setTimeout(checkIfResourceHasBeenLoaded, resourceCheckInterval);
+    };
+
+    checkIfResourceHasBeenLoaded();
+  });
+}
+
+Cypress.Commands.add('waitForResource', waitForResourceToLoad);
