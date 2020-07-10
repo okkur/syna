2e5a8c01628c6f78942c0570fbed4f76339ec48c
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2020-07-10 21:47:14 +0430

    Add postcss and purgecss to demo

---
+++ exampleSite/postcss.config.js
@@ -0,0 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [ './hugo_stats.json' ],
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
+    }
+});
+
+module.exports = {
+  plugins: [
+      ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
+  ]
+};
