721dab5dfb90cf5224ed49a1d854c4d1ab409b95
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2020-07-12 13:58:11 +0430

    Move purgecss to exampleSite

--- exampleSite/postcss.config.js
+++ exampleSite/postcss.config.js
@@ -1,13 +1,13 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [ './hugo_stats.json' ],
-    defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-    }
+  content: [ './hugo_stats.json' ],
+  defaultExtractor: (content) => {
+      let els = JSON.parse(content).htmlElements;
+      return els.tags.concat(els.classes, els.ids);
+  }
 });
 
 module.exports = {
-  plugins: [
-      ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
-  ]
+plugins: [
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
+]
 };
