f49a72954bc5260e0000368b17d084849e0e8a46
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2020-07-13 20:25:57 +0430

    Update purgecss and some styles to make demo smaller and fix tests

--- exampleSite/postcss.config.js
+++ exampleSite/postcss.config.js
@@ -1,13 +1,17 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [ './hugo_stats.json' ],
+  content: ['./hugo_stats.json'],
   defaultExtractor: (content) => {
-      let els = JSON.parse(content).htmlElements;
-      return els.tags.concat(els.classes, els.ids);
-  }
+    let els = JSON.parse(content).htmlElements;
+    return els.tags.concat(els.classes, els.ids, [
+      'active',
+      'show',
+      'in',
+    ]);
+  },
 });
 
 module.exports = {
-plugins: [
-    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
-]
+  plugins: [
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : []),
+  ],
 };
